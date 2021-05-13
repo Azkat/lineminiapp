b6f014178fc067c6c4137c2d4747ce309276331f
Azkat <azkat.sg@gmail.com>
2021-05-13 17:17:56 +0900

    scan qr

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
+window.onload = function() {
+  const defaultLiffId = process.env.API_KEY;
+  initializeLiff(defaultLiffId);
+};
+
+function initializeLiff(myLiffId) {
+  liff
+  .init({
+      liffId: myLiffId
+  })
+  .then(() => {
+      liff.scanCode().then(result => {
+          const stringifiedResult = result.value;
+          liff.sendMessages([{
+              'type': 'text',
+              'text': stringifiedResult
+          }]).then(() => {
+              liff.closeWindow();
+          }).catch((error) => {
+              window.alert('Error sending message: ' + error);
+          });
+      }).catch(err => {
+          window.alert('scanCode failed!');
+      });
+  })
+  .catch((err) => {
+      window.alert('Something went wrong with LIFF initialization.');
+  });
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!
+          Edit <code>src/App.js</code> and save to reload
         </p>
         <a
           className="App-link"
