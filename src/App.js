f96586e0526f8492e16aef7860e0974b57c8acf6
Azkat <azkat.sg@gmail.com>
2021-05-13 17:34:06 +0900

    line

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import liff from '@line/liff';
 
+window.onload = function() {
+  const defaultLiffId = "1655976024-YbEzZbBX";
+  initializeLiff(defaultLiffId);
+};
+
+function initializeLiff(myLiffId) {
+  liff
+  .init({
+      liffId: myLiffId
+  })
+  .then(() => {
+      liff.scanCode().then(result => {
+          const stringifiedResult = result.value;
+          liff.sendMessages([{
+              'type': 'text',
+              'text': stringifiedResult
+          }]).then(() => {
+              liff.closeWindow();
+          }).catch((error) => {
+              window.alert('Error sending message: ' + error);
+          });
+      }).catch(err => {
+          window.alert('scanCode failed!');
+      });
+  })
+  .catch((err) => {
+      window.alert('Something went wrong with LIFF initialization.');
+  });
+}
 
 function App() {
   return (
